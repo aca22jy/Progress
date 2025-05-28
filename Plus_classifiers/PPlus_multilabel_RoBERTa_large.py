91724e001b241391eee8cbbbeb72d16f2b35b319
Tony <1372298347@qq.com>
2025-05-28 01:27:34 +0100

    change some parameters

--- Plus_classifiers/PPlus_multilabel_RoBERTa_large.py
+++ Plus_classifiers/PPlus_multilabel_RoBERTa_large.py
@@ -18,9 +18,9 @@ device = 'cuda' if cuda.is_available() else 'cpu'
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--test", default = False, action='store_true')
-parser.add_argument("--epoch", "-e", default=10, type=int)
+parser.add_argument("--epoch", "-e", default=30, type=int)
 parser.add_argument("--max_len", "-m", default=512, type=int)
-parser.add_argument("--learning_rate", "-l", type=float, default=1e-05)
+parser.add_argument("--learning_rate", "-l", type=float, default=5e-06)
 parser.add_argument("--train_batch_size", "-t", default=28, type=int)
 parser.add_argument('--journal_name', '-j', action = 'store_true')
 parser.add_argument("--bert_model", "-b", default='roberta-large')  # Change default value
